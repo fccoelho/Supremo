6d948950132b52348a03d27da11e26637297b778
Flávio C. Coelho <fccoelho@gmail.com>
2011-02-07 15:46:41 -0200

    adicionei folder SQL

--- clean/limpa.py
+++ clean/limpa.py
@@ -1,13 +1,30 @@
 import difflib
+from collections import defaultdict
+import re
 #from sqlalchemy.ext.sqlsoup import SqlSoup
 from BeautifulSoup import BeautifulSoup
 #db = SqlSoup('mysql://root:password@emapserv/Supremo')
 #print db.t_decisoes.all()
 import MySQLdb
+
+#Configura conexoes
 db=MySQLdb.connect(host="emapserv", user="root", passwd="password",db="Supremo")
 cur=db.cursor()
 
-
+def busca_UF(texto):
+    """
+    localiza e extrai referencias a unidade federal 
+    que originou o processo
+    """
+    rawstr = r"""(UF-(\D\D))+"""
+    embedded_rawstr = r"""(UF-(\D\D))+"""
+    compile_obj = re.compile(rawstr)
+    match_obj = compile_obj.search(texto)
+    if match_obj:
+#        print match_obj.groups()
+        return match_obj.groups()[1]
+    
+    
 def conta_campos(cursor):
     cursor.execute('select decisao from t_decisoes limit 10000')
     decisoes = cursor.fetchmany(10000)
@@ -24,6 +41,19 @@ def conta_campos(cursor):
         campos.update(cs)
     return campos
     
+def extrai_dados(cursor):
+    """
+    Constroi nova tabela com Datas, Estado e leis referenciadas
+    """
+    cursor.execute('select decisao,data_publicacao,data_decisao from t_decisoes limit 100,1000')
+    dados = cursor.fetchmany(10)
+    for d in dados:
+        s = BeautifulSoup(d[0].strip('[]'),  fromEncoding='IBM855')
+        c = s.findAll('pre')
+        UFs = [busca_UF(t.contents[0]) for t in c]
+        print UFs
+#        print unicode(c),  type(c)
     
 if __name__ == "__main__":
-    print conta_campos(cur)        
+#    print conta_campos(cur)
+    extrai_dados(cur)
