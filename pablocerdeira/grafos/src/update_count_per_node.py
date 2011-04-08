5f506d3b7b4a722bfb635d508e81f26f27999ea7
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2011-04-08 15:48:30 -0300

    Comentei o erro em update_count_per_node.py

--- pablocerdeira/grafos/src/update_count_per_node.py
+++ pablocerdeira/grafos/src/update_count_per_node.py
@@ -26,15 +26,23 @@ i = 0
 j = 1
 lei_atual = lei_antiga = 0
 
-while (i < (cursor.rowcount-2)):
+while i < cursor.rowcount-2:
     id, lei_1, lei_2 = cursor.fetchone()
     lei_atual = lei_1
-    if (lei_atual <> lei_antiga):
+    if lei_atual != lei_antiga:
         lei_antiga = lei_atual
         j = 1
         # print i, "Lei id 1: ", lei_1, "Lei id 2", lei_2
         sqlstr = "update gr_lei_lei set count_lei = " + str(j) + " where edge_id = " + str(id)
         print sqlstr
+        '''
+        A linha abaixo executa, mas trava o script. 
+        A execução fica travada no MySQLdb/cursors.py linha 282.
+        282 > self._do_get_result()
+        Por alguma razão quando ele executa a query ele fica travado.
+        Deve ser algum bug no meu MySQLdb.
+        Flávio, você poderia tentar rodar por ai, por favor?
+        ''' 
         cursor2.execute(sqlstr)
     else:
         j += 1
