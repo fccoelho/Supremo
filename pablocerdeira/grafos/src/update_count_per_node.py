043d1c889f8ab333099b34f4eaae78900b3fea2a
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2011-04-08 12:49:20 -0300

    Primeira verso (bugada) do update_count_per_node.py

--- pablocerdeira/grafos/src/update_count_per_node.py
+++ pablocerdeira/grafos/src/update_count_per_node.py
@@ -10,31 +10,38 @@ conn = MySQLdb.connect (host = "E04324",
                         )
    
 cursor = conn.cursor()
-cursor2 = conn.cursor()
 
-cursor.execute("select edge_id, lei_id_1, lei_id_2 from gr_lei_lei limit 0,100000")
+conn2 = MySQLdb.connect (host = "E04324", 
+                        user = "pablo",
+                        passwd = "pablo",
+                        db = "SEN_Grafo"
+                        )
+
+cursor2 = conn2.cursor()
+
+cursor.execute("select edge_id, lei_id_1, lei_id_2 from gr_lei_lei")
 
 print "Iniciando fetch"
 i = 0
 j = 1
-lei_atual = lei_antiga = cursor.fetchone()[0]
+lei_atual = lei_antiga = 0
 
-while (i < (cursor.rowcount-1)):
+while (i < (cursor.rowcount-2)):
     id, lei_1, lei_2 = cursor.fetchone()
     lei_atual = lei_1
     if (lei_atual <> lei_antiga):
         lei_antiga = lei_atual
         j = 1
-        print i, "Lei id 1: ", lei_1, "Lei id 2", lei_2
-        sqlstr = "update gr_lei_lei set count_lei = ", j, "where edge_id = ", id
+        # print i, "Lei id 1: ", lei_1, "Lei id 2", lei_2
+        sqlstr = "update gr_lei_lei set count_lei = " + str(j) + " where edge_id = " + str(id)
         print sqlstr
-        # cursor2.execute(sqlstr)
+        cursor2.execute(sqlstr)
     else:
         j += 1
-        print i, "Lei id 1: ", lei_1, "Lei id 2", lei_2
-        sqlstr = "update gr_lei_lei set count_lei = ", j, "where edge_id = ", id
+        # print i, "Lei id 1: ", lei_1, "Lei id 2", lei_2
+        sqlstr = "update gr_lei_lei set count_lei = " + str(j) + " where edge_id = " + str(id)
         print sqlstr
-        # cursor2.execute(sqlstr)
+        cursor2.execute(sqlstr)
     i += 1
 
 cursor.close()
