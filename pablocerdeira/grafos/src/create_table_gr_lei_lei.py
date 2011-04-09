70299a971d8ecd8bfe288fb249ebbf9289599fd7
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2011-04-09 01:23:45 -0300

    Mudei o create_table_gr_lei_lei.py . Agora ele cria todas as queries localmente e ao final envia tudo para o servidor. Isso proporcionou um ganho gigantesto de performance.

--- pablocerdeira/grafos/src/create_table_gr_lei_lei.py
+++ pablocerdeira/grafos/src/create_table_gr_lei_lei.py
@@ -18,6 +18,14 @@ Tabela gr_lei_lei
 Autores:
     Pablo Cerdeira
     Flávio Coelho
+    
+Notas:
+    - v0.2:
+        Nesta versão ao invés de executar individualmente cada inset,
+        optei por montar todas as queries de insert localmente, na 
+        variável sqlstr, e enviar todo de uma única vez.
+        Com isso gnhamos muito em performance, já que redizumos drasticamente
+        a comunicação com o MySQL.
 
 '''
 import MySQLdb
@@ -34,6 +42,7 @@ cursor2 = conn.cursor()
 '''
 Script para criar a tabela gr_lei_lei
 '''
+print "Criando a tabela gr_lei_lei"
 sqlstr = '''
 CREATE  TABLE gr_lei_lei (
   edge_id INT NOT NULL AUTO_INCREMENT ,
@@ -44,6 +53,7 @@ CREATE  TABLE gr_lei_lei (
   PRIMARY KEY (edge_id) );
 '''
 cursor1.execute(sqlstr)
+print "Concluída a criação da tabela."
 
 '''
 Pega todos os lei_id da tabela lei que não tenham a coluna id_correto
@@ -51,16 +61,20 @@ preenchido (com id_correto significa que a lei está cadastrada com erro)
 É importante pegar na ordem crescente para evitarmos referências duplicadas
 na tabela final (lei_id_1 <-> lei_id_2 e lei_id_2 <-> lei_id_1). 
 '''
+print "Pegando o id de cada uma das leis..."
 sqlstr = "select id from lei order by id"
 cursor1.execute(sqlstr)
+print "Ok, ids das leis em memória..."
 
 '''
 Loop executado uma vez para cada id da tabela lei detectado.
 '''
+print "Iniciando a montagem das queries em memória..."
 i = 1
+sqlstr = ""
 while i < cursor1.rowcount-1:
     lei_id_1 = cursor1.fetchone()
-    sqlstr = '''
+    sqlstr = sqlstr + '''
         insert into gr_lei_lei (lei_id_1, lei_id_2, peso) 
             select %s lei_id_1, ld_1.lei_id lei_id_2, count(ld_1.lei_id) peso 
             from lei_decisao ld_1 
@@ -72,9 +86,17 @@ while i < cursor1.rowcount-1:
                 ) 
                 and ld_1.lei_id > %s 
             group by ld_1.lei_id 
-            order by count(ld_1.lei_id) desc;
+            order by count(ld_1.lei_id) desc; 
         ''' % (str(i), str(i), str(i))
-    print "Inserindo lei_id = %s" % str(i)
-    cursor2.execute(sqlstr)
+    print "Inserindo lei_id = %s na query" % str(i)
+    # cursor2.execute(sqlstr)
     i += 1
+    
+print "Montagem da SqlStr concluída"
+print "Iniciando o send da query"
+cursor2.execute(sqlstr)
+print "Concluído!!!"
 
+cursor.close()
+
+conn.close()
