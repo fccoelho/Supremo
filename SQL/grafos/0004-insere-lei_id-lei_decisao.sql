b7701fcd06406262263adee4fe715e85836c6d09
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2011-04-13 00:40:35 -0300

    Fiz uma série de alterações nos scripts SQL para geração das tabelas da instância SEN_Grafo. Renomeei todos os scripts (SQL e Python) incluíndo uma numeração no início, indicadora de ordem de execução dos scripts. Para gerar todas as tabelas do zero, executar primeiro os SQLs e depois os Python, seguindo a sua ordem de numeração.

---
+++ SQL/grafos/0004-insere-lei_id-lei_decisao.sql
@@ -0,0 +1,62 @@
+/* 
+Autor: Pablo Cerdeira
+
+Insere os valores de lei_id na tabela lei_decisao.
+
+*/
+
+/* 
+Inserindo primeiro os valores de id da tabela lei, mesmo
+que incorretos.
+ATENÇÃO: se a tabela lei_decisao e a tabela lei estiverem
+sem índices nas colunas esfera, lei e ano esta query é 
+bastante demorada.
+Por isso antes vamos inserir os índices em ambas.
+*/
+
+ALTER TABLE lei
+      ADD INDEX ix_esfera (esfera ASC), 
+      ADD INDEX ix_lei (lei ASC), 
+      ADD INDEX ix_ano (ano ASC);
+
+ALTER TABLE lei_decisao
+      ADD INDEX ix_esfera (esfera ASC), 
+      ADD INDEX ix_lei (lei ASC), 
+      ADD INDEX ix_ano (ano ASC);
+
+UPDATE
+	lei_decisao ld,
+	lei l
+SET ld.lei_id = l.id
+WHERE
+	l.esfera = ld.esfera AND
+	l.lei = ld.lei AND
+	l.ano = ld.ano;
+
+/*
+Agora fazendo a atualização dos lei_id na tabela lei_decisao
+para os casos em que a lei está incorretamente grafada.
+Ou seja, pegamos os valores id_correto da tabela lei.
+*/
+UPDATE
+	lei_decisao ld,
+	lei l
+SET ld.lei_id = l.id_correto
+WHERE
+	l.esfera = ld.esfera AND
+	l.lei = ld.lei AND
+	l.ano = ld.ano AND
+	l.id_correto IS NOT NULL;
+
+/*
+ATENÇÃO: É importante criar o índice para para lei_id, 
+do contrário a execução dos scripts em Python serão seriamente
+afetadas.
+*/
+ALTER TABLE lei_decisao
+      ADD INDEX ix_lei_id (lei_id ASC)
+
+/*
+Concluída esta etapa, o próximo passo é executar os scripts
+Python disponíveis no diretório /pablocerdeira/grafos/src
+*/
