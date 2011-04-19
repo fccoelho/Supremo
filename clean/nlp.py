81c3bf8bc402345f099a00d90996735f71c5c879
Renato Rocha Souza <renato@E04678.(none)>
2011-04-19 11:03:10 -0300

    pequenas mudancas

--- clean/nlp.py
+++ clean/nlp.py
@@ -8,39 +8,61 @@ from MySQLdb.cursors import CursorUseResultMixIn,  DictCursor, SSDictCursor,  SS
 
 db=MySQLdb.connect(host="E04324", user="root", passwd="password",db="Supremo")
 cur=db.cursor()
+reg_inicio = 0
+num_reg = 100
+consulta_banco = 'select decisao from Supremo.t_decisoes limit %s,%s;'%(reg_inicio, num_reg)
+match_string1 = r"""(?u)UNÂN|UNAN|unan|unân"""
+match_string2 = "MAIORIA"
 
-def analisa_votacao(cursor):
+def busca_expressao(cursor,  querydb, rawstr,  num_registros):
     """
-    Analisa os resultados da votacao, buscando por
-    ocorrencias dos lexemas derivados dos morfema
-    'unam', como unanime, unanimidade, unanimamente;
-    e o lexema 'maioria', que indica uma votacao não unanime
+    Recebe uma expressao e uma consulta e busca por ocorrencias
+    da expressao nos resultados da consulta
     """
-    #expressoes a serem encontradas
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') 
-    rawstr1 = r"""(?u)UNÂN|UNAN|unan|unân"""
-    rawstr2 = "MAIORIA"
-    num_decisoes = 10000
+    compile_obj = re.compile(rawstr, re.LOCALE)
+    cursor.execute(querydb)
+    registros = cursor.fetchmany(num_registros)
+    matches = 0
+    # extraindo expressoes dos dados recuperados
+    for d in registros:
+        sopa = BeautifulSoup(d[0].strip('[]'))#,  fromEncoding='ISO8859-1')
+        texto = sopa.findAll('pre') 
+        match_obj = compile_obj.findall(str(texto))
+        if match_obj: matches +=1
+    #print matches,  match_obj
+    return matches,  match_obj
+    cursor.close()
+
+def busca_expressoes(cursor,  querydb, rawstr1,  rawstr2,  num_registros):
+    """
+    Recebe expressoes e uma consulta e busca por ocorrencias
+    destas nos resultados da consulta
+    """
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8') 
     compile_obj1 = re.compile(rawstr1, re.LOCALE)
     compile_obj2 = re.compile(rawstr2, re.LOCALE)
-    #buscando dados no banco
-    cursor.execute('select decisao from Supremo.t_decisoes limit 10000')
-    decisoes = cursor.fetchmany(num_decisoes)
+    cursor.execute(querydb)
+    registros = cursor.fetchmany(num_registros)
     matches1 = 0
     matches2 = 0
-    #extraindo expressoes dos dados recuperados
-    for d in decisoes:
-        sopa = BeautifulSoup(d[0].strip('[]'),  fromEncoding='ISO8859-1')
+    # extraindo expressoes dos dados recuperados
+    for d in registros:
+        sopa = BeautifulSoup(d[0].strip('[]'))#,  fromEncoding='ISO8859-1')
         texto = sopa.findAll('pre') 
         match_obj1 = compile_obj1.findall(str(texto))
         match_obj2 = compile_obj2.findall(str(texto))
+        if not match_obj1 and not match_obj2: print texto   
         if match_obj1: matches1 +=1
         if match_obj2: matches2 +=1
-        #print match_obj
-    print 'Em', num_decisoes, 'decisoes, foram identificadas'
-    print matches1, 'decisoes unanimes', '(', (100*matches1)/(matches1+matches2),'%) e' 
-    print matches2, 'decisoes por maioria', '(', (100*matches2)/(matches1+matches2),'%).'
+    print matches1,  match_obj1
+    print matches2,  match_obj2
+    #return matches,  match_obj
     cursor.close()
 
-analisa_votacao(cur)
+#numero,  expressao = busca_expressao(cur, consulta_banco, match_string2, num_reg)
+#print numero,  expressao
+
+busca_expressoes(cur, consulta_banco,  match_string1, match_string2,  num_reg)
+
 db.close()
