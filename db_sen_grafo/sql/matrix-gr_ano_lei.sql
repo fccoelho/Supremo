f2e590d5c8500a46649740b6c72e11bd12def85d
Pablo Cerdeira <pablo.cerdeira@gmail.com>
2011-04-21 11:01:53 -0300

    Alguns scripts para geração de tabelas de grafos (gr_ministro_lei e gr_ano_lei).

---
+++ db_sen_grafo/sql/matrix-gr_ano_lei.sql
@@ -0,0 +1,22 @@
+/*
+Cria tabela gr_ano_lei
+*/
+USE SEN_Grafo;
+
+DROP TABLE IF EXISTS gr_ano_lei;
+
+CREATE TABLE
+  gr_ano_lei 
+SELECT 
+  t.ano origid, lei_id destid, count(lei_id) weight
+FROM
+  (
+  SELECT DISTINCT 
+    id_processo, year(data_decisao) ano, id_decisao, lei_id
+  FROM 
+    SEN_Grafo.matrix
+  ) t
+GROUP BY
+  t.ano
+ORDER BY
+  count(t.lei_id) DESC;
--- db_sen_grafo/sql/matrix_testes.sql
+++ db_sen_grafo/sql/matrix_testes.sql
@@ -32,3 +32,40 @@ WHERE
   artigo_id IS NOT NULL
 ORDER BY
   id_decisao;
+
+/*
+Terceiro teste.
+Incluíndo relator na tabela semelhante à
+lei_decisao.
+Funcionou também, como esperado.
+*/
+SELECT DISTINCT 
+  id_processo, relator, id_decisao, lei_id
+FROM 
+  SEN_Grafo.matrix
+WHERE
+  lei_id_artigo IS NOT NULL OR
+  artigo_id IS NOT NULL
+ORDER BY
+  id_decisao;
+
+/*
+Quarto teste.
+Gerando grafo ministro_lei
+*/
+SELECT 
+  relator, lei_id, count(lei_id) citacoes
+FROM
+  (
+  SELECT DISTINCT 
+    id_processo, relator, id_decisao, lei_id
+  FROM 
+    SEN_Grafo.matrix
+  WHERE
+    lei_id_artigo IS NOT NULL OR
+    artigo_id IS NOT NULL
+  ) t
+GROUP BY
+  t.relator
+ORDER BY
+  count(t.lei_id) DESC;
