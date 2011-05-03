7eaf9b0952cb028f9b647d0adefc3b09930fd3bf
Augusto Nasser <tn.augusto@gmail.com>
2011-05-03 02:41:35 -0300

    atualiza

--- visualiza/classes_com_ano.py
+++ visualiza/classes_com_ano.py
@@ -3,6 +3,7 @@
 import ubigraph
 import csv
 import time
+from math import log
 
 u = ubigraph.Ubigraph()
 u.clear()
@@ -27,24 +28,31 @@ ano = 1988
 x = u.newVertex(color='#ff0000', shape='cube', label=str(ano))
 vertexAno[ano] = x
 
+#for i in range(1989,2010):
+#    x = u.newVertex(color='#ff0000', shape='cube', label=str(i))
+#    vertexAno[i] = x
+#    u.newEdge(vertexAno[i-1], x, strength=10,
+#              visible='false')
+
 for row in f:
     if row[0] == 'corte':
         continue
     
     if ano != int(row[2]):
         ano += 1
-        time.sleep(4)
+        time.sleep(1)
         x = u.newVertex(color='#ff0000', shape='cube', label=str(ano))
         vertexAno[ano] = x
+        u.newEdge(vertexAno[ano-1], x, strength=-1, visible='false')
         
         
     if row[1] not in vertexClasse:
-        y = u.newVertex(color='#0000ff', shape='sphere', label=labels[row[1]])
-#                        size=int(row[3]))
-        vertexClasse[row[1]] = y
+        y = u.newVertex(color='#0000ff', shape='sphere', label=labels[row[1]],
+                        size=int(row[3])*0.00001)
+        vertexClasse[row[1]] = [y, int(row[3])]
         
-    u.newEdge(vertexClasse[row[1]], vertexAno[ano], strength=int(row[3]),
+    u.newEdge(vertexClasse[row[1]][0], vertexAno[ano], strength=int(row[3]),
               visible='true')
-#    classe = vertexClasse[row[1]]
-#    classe[1] += row[3]
-#    classe[0].set(size=int(classe[1]))
+    classe = vertexClasse[row[1]]
+    classe[1] += int(row[3])
+    classe[0].set(size=int(classe[1])*0.00001)
