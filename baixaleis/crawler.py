c90e0554874276a723e58a83ddc7706e35558237
Augusto Nasser <tn.augusto@gmail.com>
2011-05-09 04:41:30 -0300

    alterações em "crawler.py"

--- baixaleis/crawler.py
+++ baixaleis/crawler.py
@@ -1,24 +1,26 @@
 # -*- coding:utf-8 -*-
 
 import csv
-from utils import get_next_tag
+from utils import get_next_tag, get_text
 
 f = open('codigo_processo_civil.htm', 'r')
-g = open('codigo_processo_civil.csv', 'r+')
+g = csv.writer(open('codigo_processo_civil.csv', 'w'))
 
-g.write('"livro","título","capítulo","seção","subseção","artigo","inciso",
-        "parágrafo","texto")
+g.writerow(["'id'","'livro'","'titulo'","'capitulo'","'secao'",
+            "'subsecao'","'artigo'","'paragrafo'","'inciso'",
+            "'alinea'","'texto'"])
 
 doc = ''
 for line in f:
     doc += line
 
-livro, titulo, capitulo, secao = 0, 0, 0, 0
-subsecao, artigo, paragrafo, inciso, alinea = 0, 0, 0, 0, 0
+idt = 0
+livro, titulo, capitulo, secao, subsecao = 0, 0, 0, 0, 0
+artigo, paragrafo, inciso, alinea = 0, 0, 0, 0
 
 l = get_next_tag(doc)
 
-while l != []
+while l != []:    
     tag, pos = l[0], l[1]
     
     if tag == '>LIVRO':
@@ -47,10 +49,40 @@ while l != []
     
     elif tag == 'Art.':
         artigo +=1
+        idt += 1
         inciso = 0
-        parágrafo = 
-        t = get_text(pos)
-        g.write('"%d","%d","%d","%d,"%d","%d","%d","%d",' %
-                (livro, titulo, capitulo, secao, subsecao, artigo, inciso,
-                 paragrafo) + '"' + t + '"')
+        paragrafo = 0
+        t = get_text(doc, pos)
+        g.writerow([idt, livro, titulo, capitulo, secao, subsecao, artigo,
+                    paragrafo, inciso, alinea, t])
         pos += 1
+    
+    elif tag == 'Parágrafo':
+        paragrafo += 1
+        idt += 1
+        inciso = 0
+        t = get_text(doc, pos)
+        g.writerow([idt, livro, titulo, capitulo, secao, subsecao, artigo,
+                    paragrafo, inciso, alinea, t])
+        pos += 1
+    
+    elif tag == 'Inciso':
+        inciso += 1
+        idt += 1
+        paragrafo = 0
+        alinea = 0
+        t = get_text(doc, pos)
+        g.writerow([idt, livro, titulo, capitulo, secao, subsecao, artigo,
+                    paragrafo, inciso, alinea, t])
+        pos += 1
+    
+    elif tag == 'Alinea':
+        alinea += 1
+        idt += 1
+        t = get_text(doc, pos)
+        g.writerow([idt, livro, titulo, capitulo, secao, subsecao, artigo,
+                    paragrafo, inciso, alinea, t])
+        pos += 1
+    
+    l = get_next_tag(doc, pos)
+    print idt
--- baixaleis/utils.py
+++ baixaleis/utils.py
@@ -3,11 +3,54 @@
 import re
 
 
-def get_next_tag(string):
-    r = re.search('>LIVRO|>TÍTULO|>CAPÍTULO|>SEÇÃO|>Subseção|Art.')
-    if r != None:
-        return [r.group(), string.find(r.group())]
-    return []
+def get_next_tag(string, inicio=0):
+    r = []
+    r.append(re.search('>LIVRO|>TÍTULO|>CAPÍTULO|>SEÇÃO|>Subseção|Art.', string[inicio:]))
+    r.append(re.search('[IVXLC]+[IVXLC]*(&nbsp;| -)', string[inicio:]))
+    r.append(re.search('§|Parágrafo Único', string[inicio:]))
+    r.append(re.search('[\n\t >][a-h]\)', string[inicio:]))
     
-def get_text:
-    pass
+    for i in range(4):
+        if i == 0:
+            menor = r[0]
+            if r[0] is None:
+                pos = len(string)
+                continue
+            pos = string.find(r[0].group(), inicio)
+        else:
+            if r[i] is None:
+                pos = len(string)
+                continue
+            if (string.find(r[i].group(), inicio)) < pos:
+                menor = r[i]
+                pos = string.find(r[i].group(), inicio)
+    
+    if menor is None:
+        return []
+    
+    if menor is r[0]:
+        return [r[0].group(), pos]
+    
+    if menor is r[1]:
+        return ['Inciso', pos]
+    
+    if menor is r[2]:
+        return ['Parágrafo', pos]
+    
+    if menor is r[3]:
+        return ['Alinea', pos]
+
+
+def get_text(string, inicio=0):
+    final = string.find('</p>', inicio)
+    ignore = re.findall('&[^;]+;|<[^>]*>|\n|\t', string[inicio:final])
+    t = ''
+    
+    ant = inicio
+    for i in range(len(ignore)):
+        prox = string.find(ignore[i], ant, final)
+        t += string[ant:prox] + ' '
+            
+        ant = prox + len(ignore[i])
+    
+    return t
